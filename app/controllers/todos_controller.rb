d96b1f6fb15981c74e88cc857339ad202c873d51
“Maribel” <maribel.sainzc@gmail.com>
2019-01-30 12:04:02 -0300

    create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,16 @@ def new
   @todo = Todo.new
 end
 
+def create
+  @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to root_path
 end
+
+private
+def todo_params
+  params.require(:todo).permit(:description, :completed)
+end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
   get 'todos/new', as: 'new_todo'
-
+  post 'todos', to: 'todos#create'
   root 'todos#index'
 end
