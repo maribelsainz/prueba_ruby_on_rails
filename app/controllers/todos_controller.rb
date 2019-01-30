4238417f507fb5570c17dc899f442af1a795004f
“Maribel” <maribel.sainzc@gmail.com>
2019-01-30 15:12:14 -0300

    todo menos landing y navbar

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,6 +13,38 @@ def create
     redirect_to root_path
 end
 
+def show
+  @todo = Todo.find(params[:id])
+end
+
+def edit
+  @todo = Todo.find(params[:id])
+end
+
+def update
+  @todo = Todo.find(params[:id])
+  @todo.update(todo_params)
+  redirect_to root_path, notice: 'El To Do a sido actualizado con éxito!'
+end
+
+def destroy
+  @todo = Todo.find(params[:id])
+  @todo.destroy
+  redirect_to root_path, notice: 'El To Do ha sido eliminado con éxito!'
+end
+
+def complete
+    @todo = Todo.find(params[:id])
+    @todo.completed = true
+    @todo.save
+    redirect_to root_path
+end
+
+# def list
+#   @todo = Todo.all
+# end
+
+
 private
 def todo_params
   params.require(:todo).permit(:description, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
   get 'todos/new', as: 'new_todo'
   post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  put 'todos/:id', to: 'todos#update'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get 'todos/:id/complete', to: 'todos#complete', as: 'complete_todo'
+  # get 'todos/:id/list', to: 'todos#list' as: 'list_todo'
   root 'todos#index'
+
 end
